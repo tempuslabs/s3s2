808380bf9e7e139ba8ed9ce0c4af1de4148b350e
Colin Smith <colin.smith@tempus.com>
2019-10-30 14:03:09 -0500

    ls plz

--- deploy_build.sh
+++ deploy_build.sh
@@ -14,3 +14,5 @@ echo "Building for operating systems..."
 GOOS=linux GOARCH=amd64 GOCACHE=$WORKSPACE go build -o linux/s3s2-linux-amd64 -v &
 GOOS=darwin GOARCH=amd64 GOCACHE=$WORKSPACE go build -o darwin/s3s2-darwin-amd64 -v &
 GOOS=windows GOARCH=amd64 GOCACHE=$WORKSPACE go build -o windows/s3s2-windows-amd64.exe -v &
+
+ls
