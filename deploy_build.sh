f9a437582339b1cf3b048b50b3f99a300008a2c6
Colin Smith <colin.smith@tempus.com>
2019-10-30 14:36:10 -0500

    removing the &

--- deploy_build.sh
+++ deploy_build.sh
@@ -10,8 +10,7 @@ echo "Installing from local Go mod..."
 GOCACHE=$WORKSPACE go mod download
 
 echo "Building for operating systems..."
-GOOS=linux GOARCH=amd64 GOCACHE=$WORKSPACE go build -o linux/s3s2-linux-amd64 -v &
-GOOS=darwin GOARCH=amd64 GOCACHE=$WORKSPACE go build -o darwin/s3s2-darwin-amd64 -v &
-GOOS=windows GOARCH=amd64 GOCACHE=$WORKSPACE go build -o windows/s3s2-windows-amd64.exe -v &
-sleep 5
+GOOS=linux GOARCH=amd64 GOCACHE=$WORKSPACE go build -o linux/s3s2-linux-amd64 -v
+GOOS=darwin GOARCH=amd64 GOCACHE=$WORKSPACE go build -o darwin/s3s2-darwin-amd64 -v
+GOOS=windows GOARCH=amd64 GOCACHE=$WORKSPACE go build -o windows/s3s2-windows-amd64.exe -v
 
